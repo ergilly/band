4e200b6fe636bf3fcec4a7a14b71128b629af93d
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-04-04 11:40:36 +0100

    beautify files

--- band.js
+++ band.js
@@ -1,8 +1,28 @@
-const bob = {name: 'bob', instrument: 'Bass', age: 24};
-const bill = {name: 'bill', instrument: 'Guitar', age: 27};
-const brenda = {name: 'brenda', instrument: 'Keyboard', age: 23};
-const belatrix = {name: 'belatrix', instrument: 'lead vocals', age: 26};
-const betty = {name: 'betty', instrument: 'drums', age: 84};
+const bob = {
+  name: 'bob',
+  instrument: 'Bass',
+  age: 24
+};
+const bill = {
+  name: 'bill',
+  instrument: 'Guitar',
+  age: 27
+};
+const brenda = {
+  name: 'brenda',
+  instrument: 'Keyboard',
+  age: 23
+};
+const belatrix = {
+  name: 'belatrix',
+  instrument: 'lead vocals',
+  age: 26
+};
+const betty = {
+  name: 'betty',
+  instrument: 'drums',
+  age: 84
+};
 
 const members = [bob, bill, brenda, belatrix, betty];
 
